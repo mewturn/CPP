b1f23339e32395bf5baa887677d638483baa11a1
mewturn <pathonice@hotmail.com>
2017-05-30 21:55:39 +0800

    Simple addition
    Simple programme adding two inputs "x" and "y" together to return "x+y".

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int x;
+	int y;
+
+	cout << "Enter x: ";
+	cin >> x;
+
+	cout << "Enter y: ";
+	cin >> y;
+
+	cout << "x = " << x << endl;
+	
+	cout << "y = " << y << endl;
+	cout << "x + y = " << x + y << endl;
+}
